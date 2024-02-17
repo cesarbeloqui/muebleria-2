ede3379119ddd2af1f318482ebfb1ac88c288be8
cesarbeloqui <beloqui.cesar@gmail.com>
2024-02-16 21:57:36 -0300

    image de fondo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,25 @@
-import React from "react";
 import {
   FeaturedProducts,
   Hero,
-  Services,
-  Contact,
+  /*   Services,
+  Contact, */
   Loading,
 } from "../components";
 import styled from "styled-components";
 import fondoHeader from "../assets/fondoHeader.png";
+import { useEffect } from "react";
 import { useState } from "react";
 
 const HomePage = () => {
-  return fondoHeader ? (
-    <Wrapper backgroundImage={fondoHeader}>
+  const [loaded, setLoaded] = useState(false);
+
+  // Simula un tiempo de carga
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return loaded ? (
+    <Wrapper>
       <main className="main">
         <Hero />
         <FeaturedProducts />
@@ -24,17 +31,18 @@ const HomePage = () => {
     <Loading />
   );
 };
-const Wrapper = styled.section`
+const Wrapper = styled.section.attrs((props) => ({
+  style: {
+    background: `linear-gradient(#0000004e, #000000c5), url(${fondoHeader}) fixed`,
+    backgroundAttachment: "fixed",
+    marginTop: "2rem",
+  },
+}))`
   /*   min-height: 60vh;
   display: grid;
   place-items: center;
   margin-top: 3rem; */
   /*   background-image: url(${fondoHeader});*/
-  .main {
-    background-size: cover;
-    background-image: url(${(props) => props.backgroundImage});
-    background-position: center;
-  }
 `;
 
 export default HomePage;
